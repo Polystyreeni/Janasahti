982b24157fc1f01298f9fc1609d496b8a20199e9
Polystyreeni <jesperi.tolvanen@tuni.fi>
2023-01-21 00:44:44 +0200

    Added function to calculate text size from screen size

--- Wordgame/app/src/main/java/com/example/wordgame/TextUtils.java
+++ Wordgame/app/src/main/java/com/example/wordgame/TextUtils.java
@@ -3,8 +3,14 @@ package com.example.wordgame;
 import android.os.Build;
 import android.text.Html;
 import android.text.Spanned;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
 
 public class TextUtils {
+
+    public static final String TAG = "TextUtils";
+
     public static Spanned getSpannedText(String text) {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             return Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT);
@@ -12,5 +18,16 @@ public class TextUtils {
         else {
             return Html.fromHtml(text);
         }
+    }
+
+    public static int getTileTextSize(WindowManager windowManager) {
+        DisplayMetrics dm = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(dm);
+
+        double width = dm.widthPixels / dm.xdpi;
+        Log.d(TAG, "The width of screen is: " + width);
+
+        // Totally 100% arbitrary equation that has not been tested at all
+        return (int)(width * 12);
     }
 }
