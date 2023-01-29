f364d37e7cca3523b6b2e1acaf0ce84a67cd50a7
Polystyreeni <jesperi.tolvanen@tuni.fi>
2023-01-29 18:32:31 +0200

    Clean-up of code

--- Wordgame/app/src/main/java/com/example/wordgame/TextUtils.java
+++ Wordgame/app/src/main/java/com/example/wordgame/TextUtils.java
@@ -1,11 +1,9 @@
 package com.example.wordgame;
 
-import android.graphics.Point;
 import android.os.Build;
 import android.text.Html;
 import android.text.Spanned;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.WindowManager;
 
 public class TextUtils {
@@ -26,28 +24,8 @@ public class TextUtils {
         windowManager.getDefaultDisplay().getMetrics(dm);
 
         double width = dm.widthPixels / dm.xdpi;
-        //double height = dm.heightPixels / dm.ydpi;
-        //double baseSize = Math.max(width, height);
-
-        //Log.d(TAG, "The width of screen is: " + width);
 
         // Totally 100% arbitrary equation that has not been tested at all
         return (int)(width * 14);
     }
-
-    /*public static double getScreenWidth(WindowManager windowManager) {
-        DisplayMetrics dm = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(dm);
-
-        final Point size = new Point();
-        windowManager.getDefaultDisplay().getRealSize(size);
-
-        double width = dm.widthPixels;
-        return width;
-
-        //return size.x;
-
-        //double width = dm.widthPixels / dm.xdpi;
-        //return width;
-    }*/
 }
