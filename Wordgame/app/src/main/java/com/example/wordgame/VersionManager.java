92d8378e24d0106f0bafb42f2e83586ab2b3063c
Polystyreeni <jesperi.tolvanen@gmail.com>
2023-06-14 20:21:53 +0300

    Version changed to 1.5.1

--- Wordgame/app/src/main/java/com/example/wordgame/VersionManager.java
+++ Wordgame/app/src/main/java/com/example/wordgame/VersionManager.java
@@ -11,7 +11,7 @@ import com.android.volley.toolbox.Volley;
 
 public class VersionManager {
     private static final String TAG = "VersionManager";
-    private static final String version = "1.5.0";
+    private static final String version = "1.5.1";
 
     // Will be overridden once version file is fetched
     private static String latestVersion = "1.5.0";
